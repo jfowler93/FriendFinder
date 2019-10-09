5622b79086a0412fe089aa9b5e2a357b7eb97e10
Josh F <fowlerjosh93@gmail.com>
2019-10-09 18:23:02 -0400

    created intial files, set up server.js, added three friends to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friends =[
+
+    {
+        name: "Frank Reynolds",
+        photo: "",
+        scores: [
+            "5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+        ]
+    },
+    {
+        name: "Michael Scott",
+        photo: "",
+        scores: [
+            "3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"	
+        ]
+    },
+    {
+        name: "North Hollywood Henry",
+        photo: "",
+        scores: [
+            "3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+        ]
+    },
+]
